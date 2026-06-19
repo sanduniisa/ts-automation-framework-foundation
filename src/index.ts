1c9d8a5ea47efeea1b6d475dcad7cabb37e7bb73
sanduniisa <sanduniisa@gmail.com>
2026-06-20 00:44:22 +0400

    Adding Exersice 03

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {lockedOutUser ,standardUser} from './test-data/users';
 import {createUser} from './factories/userFactory';
-import {printUserScenario} from './utils/printUtils';
+import {printProductSummary, printUserScenario} from './utils/printUtils';
+import {Product} from './types/product.types';
+import {createProduct} from './factories/productFactory';
 
 const adminUser = createUser({
     username: 'admin_user',
@@ -11,3 +13,16 @@ const users = [standardUser, lockedOutUser, adminUser];
 for (const user of users) {
     printUserScenario(user);
 }
+
+const products : Product[] = [
+    createProduct(),
+    createProduct({id: 19002, name: 'product Bike Light', price: 5000, category: 'bike-light'}),
+    createProduct({id: 19003, name: 'product T-Shirt', price: 1500, category: 't-shirt'}),
+    createProduct({id: 19004, name: 'product Backpack', price: 3000, category: 'backpack'}),
+];
+
+for (const product of products) {
+    printProductSummary(product);
+}
+   
+
--- src/utils/printUtils.ts
+++ src/utils/printUtils.ts
@@ -1,5 +1,10 @@
 import {TestUser} from '../types/user.types';
+import {Product} from '../types/product.types';
 
 export function printUserScenario(user: TestUser): void {
     console.log(`Testing with user: ${user.username} as: ${user.role}`);
+}
+
+export function printProductSummary(product: Product): void {
+    console.log(`${product.name} costs ${product.price}`);
 }
